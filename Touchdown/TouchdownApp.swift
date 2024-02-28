4d7656d45ebceebd6ba58c71b63ef4b347531c24
Abhyuday Reddy Nandikonda <abhyudayreddy1534@gmail.com>
2024-02-28 13:53:29 -0600

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Sravanthi Chinthireddy on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
